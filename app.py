40262c45c7ce16309f40726805f878a86fbd7295
Shadman Yaser <shadmanyaser890@gmail.com>
2025-07-26 03:45:49 +0600

    Update app.py

--- app.py
+++ app.py
@@ -136,5 +136,9 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
